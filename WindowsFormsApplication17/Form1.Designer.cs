885421dbf0077fba12695e6c8ec953591fcbaed8
ALFREDO ANTONIO ROSALES RAYMUNDO <RR01111@ti.ues.edu.sv>
2023-09-02 20:10:51 -0500

    Solucion de programa de boleteria

---
+++ WindowsFormsApplication17/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace WindowsFormsApplication17
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cmbLocalidades = new System.Windows.Forms.ComboBox();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.lstResultados = new System.Windows.Forms.ListBox();
+            this.lblAsistentes = new System.Windows.Forms.Label();
+            this.lblMonto = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmbLocalidades
+            // 
+            this.cmbLocalidades.FormattingEnabled = true;
+            this.cmbLocalidades.Items.AddRange(new object[] {
+            "Sol Candente",
+            "Sol Luminoso",
+            "Sombrita",
+            "Tribunita",
+            "Silla Plástica"});
+            this.cmbLocalidades.Location = new System.Drawing.Point(253, 146);
+            this.cmbLocalidades.Name = "cmbLocalidades";
+            this.cmbLocalidades.Size = new System.Drawing.Size(121, 21);
+            this.cmbLocalidades.TabIndex = 0;
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(253, 173);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(100, 20);
+            this.txtCantidad.TabIndex = 1;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(552, 148);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 2;
+            this.btnAgregar.Text = "Ingresar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // lstResultados
+            // 
+            this.lstResultados.Enabled = false;
+            this.lstResultados.FormattingEnabled = true;
+            this.lstResultados.Location = new System.Drawing.Point(253, 199);
+            this.lstResultados.Name = "lstResultados";
+            this.lstResultados.Size = new System.Drawing.Size(243, 95);
+            this.lstResultados.TabIndex = 3;
+            // 
+            // lblAsistentes
+            // 
+            this.lblAsistentes.AutoSize = true;
+            this.lblAsistentes.BackColor = System.Drawing.Color.Transparent;
+            this.lblAsistentes.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAsistentes.ForeColor = System.Drawing.Color.White;
+            this.lblAsistentes.Location = new System.Drawing.Point(250, 297);
+            this.lblAsistentes.Name = "lblAsistentes";
+            this.lblAsistentes.Size = new System.Drawing.Size(107, 16);
+            this.lblAsistentes.TabIndex = 4;
+            this.lblAsistentes.Text = "Total Asistentes:";
+            // 
+            // lblMonto
+            // 
+            this.lblMonto.AutoSize = true;
+            this.lblMonto.BackColor = System.Drawing.Color.Transparent;
+            this.lblMonto.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMonto.ForeColor = System.Drawing.Color.White;
+            this.lblMonto.Location = new System.Drawing.Point(250, 325);
+            this.lblMonto.Name = "lblMonto";
+            this.lblMonto.Size = new System.Drawing.Size(82, 16);
+            this.lblMonto.TabIndex = 5;
+            this.lblMonto.Text = "Total Monto:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 146);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(179, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Seleccione Localidad";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(12, 171);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(184, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Cantidad de Entradas";
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(552, 199);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(75, 23);
+            this.btnNuevo.TabIndex = 8;
+            this.btnNuevo.Text = "Nuevo Dato";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(552, 271);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 9;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(109, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(471, 24);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Seleccione una localidad y digite cantidad de entradas.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(664, 357);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblMonto);
+            this.Controls.Add(this.lblAsistentes);
+            this.Controls.Add(this.lstResultados);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.cmbLocalidades);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbLocalidades;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.ListBox lstResultados;
+        private System.Windows.Forms.Label lblAsistentes;
+        private System.Windows.Forms.Label lblMonto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WindowsFormsApplication17/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication17
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, int> asistentesPorLocalidad = new Dictionary<string, int>();
+        private Dictionary<string, decimal> montoPorLocalidad = new Dictionary<string, decimal>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            InicializarDiccionarios();
+        }
+
+        private void InicializarDiccionarios()
+        {
+            // Inicializa los diccionarios con las localidades
+            asistentesPorLocalidad["Sol Candente"] = 0;
+            asistentesPorLocalidad["Sol Luminoso"] = 0;
+            asistentesPorLocalidad["Sombrita"] = 0;
+            asistentesPorLocalidad["Tribunita"] = 0;
+            asistentesPorLocalidad["Silla Plástica"] = 0;
+
+            montoPorLocalidad["Sol Candente"] = 0;
+            montoPorLocalidad["Sol Luminoso"] = 0;
+            montoPorLocalidad["Sombrita"] = 0;
+            montoPorLocalidad["Tribunita"] = 0;
+            montoPorLocalidad["Silla Plástica"] = 0;
+        }
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            string localidad = cmbLocalidades.Text;
+            int cantidadEntradas;
+
+            // Validar que la cantidad de entradas sea un número válido
+            if (!int.TryParse(txtCantidad.Text, out cantidadEntradas))
+            {
+                MessageBox.Show("Ingrese una cantidad válida.");
+                return;
+            }
+
+            // Validar que la localidad sea válida
+            if (!asistentesPorLocalidad.ContainsKey(localidad))
+            {
+                MessageBox.Show("Seleccione una localidad válida.");
+                return;
+            }
+
+            // Actualizar asistentes y montos por localidad
+            asistentesPorLocalidad[localidad] += cantidadEntradas;
+            montoPorLocalidad[localidad] += cantidadEntradas * ObtenerPrecioLocalidad(localidad);
+
+            // Actualizar resultados
+            MostrarResultados();
+        }
+
+        private decimal ObtenerPrecioLocalidad(string localidad)
+        {
+            // Definir los precios por localidad (ajusta estos valores según tus necesidades)
+            switch (localidad)
+            {
+                case "Sol Candente":
+                    return 3.0m;
+                case "Sol Luminoso":
+                    return 5.0m;
+                case "Sombrita":
+                    return 8.0m;
+                case "Tribunita":
+                    return 15.0m;
+                case "Silla Plástica":
+                    return 20.0m;
+                default:
+                    return 0.0m; // Si la localidad no se encuentra, regresa 0
+            }
+        }
+
+        private void MostrarResultados()
+        {
+            lstResultados.Items.Clear();
+
+            int totalAsistentes = 0;
+            decimal totalMonto = 0;
+
+            foreach (var kvp in asistentesPorLocalidad)
+            {
+                string localidad = kvp.Key;
+                int asistentes = kvp.Value;
+                decimal monto = montoPorLocalidad[localidad];
+
+                lstResultados.Items.Add($"{localidad}: {asistentes} asistentes - Monto: ${monto:F2}");
+
+                totalAsistentes += asistentes;
+                totalMonto += monto;
+            }
+
+            lblAsistentes.Text = $"Total Asistentes: {totalAsistentes}";
+            lblMonto.Text = $"Total Monto: ${totalMonto:F2}";
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            cmbLocalidades.Text = "";
+            txtCantidad.Text = "";
+            txtCantidad.Focus();
+           
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+       
+    }
+}
